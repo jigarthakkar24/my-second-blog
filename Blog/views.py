fe3e085262979a8ded28e3f4baa7ecd7a7ed4eb4
Jigar <jigar.thakkar@tops-int.com>
2016-10-12 09:50:49 +0530

    Removed env. Django ORM

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from Blog.models import Post
+from django.utils import timezone
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'Blog/post_list.html', {'posts': posts})
--- TopsBlog/settings.py
+++ TopsBlog/settings.py
@@ -53,8 +53,12 @@ WSGI_APPLICATION = 'TopsBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_tops',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
